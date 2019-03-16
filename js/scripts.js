246e4bc57f86b65cc4a2c2136ba3e509aac90e39
Stewart ilondanga <stilondanga@gmail.com>
2019-03-16 14:19:00 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,41 +18,41 @@ $(document).ready(function() {
         $('#key').val('');
         $('#add').prop('disabled', true);
       };
-      /*  const deleteProp = () => {
-          delete obj[$('select').val()];
-          updateObj(obj);
-          $('select').val('');
-          $('#delete').prop('disabled', true);
-        };
-        $('#add').click(addProp);
-        $('#delete').click(deleteProp);
-        $('input').keydown(event => {
-          if(event.keyCode == 13) addProp();
-        });
-        $('input').on('input', () => {
-          if($('input').eq(0).val() != '' && $('input').eq(1).val() != '') $('#add').prop('disabled', false);
-          else $('#add').prop('disabled', true);
-        });
-        $('select').change(event => {
-          if(event.target.value != '') $('#delete').prop('disabled', false);
-          else $('#delete').prop('disabled', true)
-        });
-      });
-      function updateObj(obj) {
-        $('#obj').empty();
-        let lastKey = '';
-        for (let y in obj) {
-          lastKey = y;
-        }
-        $('select').empty();
-        $.each(obj, (key, val) => {
-          $('<option>')
-            .attr('value', key)
-            .text(key)
-            .appendTo('select');
-          $('#obj').append('&nbsp; ' + key + ': \'' + val + '\'');
-          if(key != lastKey) $('#obj').append(',');
-          $('#obj').append('<br>');
-        });
+      const deleteProp = () => {
+        delete obj[$('select').val()];
+        updateObj(obj);
         $('select').val('');
-      }
+        $('#delete').prop('disabled', true);
+      };
+      /*        $('#add').click(addProp);
+              $('#delete').click(deleteProp);
+              $('input').keydown(event => {
+                if(event.keyCode == 13) addProp();
+              });
+              $('input').on('input', () => {
+                if($('input').eq(0).val() != '' && $('input').eq(1).val() != '') $('#add').prop('disabled', false);
+                else $('#add').prop('disabled', true);
+              });
+              $('select').change(event => {
+                if(event.target.value != '') $('#delete').prop('disabled', false);
+                else $('#delete').prop('disabled', true)
+              });
+            });
+            function updateObj(obj) {
+              $('#obj').empty();
+              let lastKey = '';
+              for (let y in obj) {
+                lastKey = y;
+              }
+              $('select').empty();
+              $.each(obj, (key, val) => {
+                $('<option>')
+                  .attr('value', key)
+                  .text(key)
+                  .appendTo('select');
+                $('#obj').append('&nbsp; ' + key + ': \'' + val + '\'');
+                if(key != lastKey) $('#obj').append(',');
+                $('#obj').append('<br>');
+              });
+              $('select').val('');
+            }
